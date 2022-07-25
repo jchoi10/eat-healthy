bb6eb07e6b3bb8496c2c48c0006dc9db9a934581
jchoi10 <noreply>
2022-07-24 22:17:13 -0400

    create recipe list button, call recipe from other API

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+// let call1 = null
+// fetch('https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=873c6628dea5448a91cb72febe1f297e')
+// .then(res => {
+//     return res.json()
+// })
+// .then(function(meals){
+//     const data = {
+//         "ingredients": [meals[0].usedIngredients[0].original]
+//     }
+//     fetch('"https://sandbox.zestfuldata.com/parseIngredients', {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(data)
+//     })
+//     .then(res => {
+//         console.log('erroring out here')
+//         console.log(res)
+//         return res.json()
+//     })
+//     .then(parsedIngredients => {
+//         console.log(parsedIngredients)
+//     })
+// })
+
+
+var recipeNames = $('#recipe-names');
+// var menus = ['ramen', 'pasta', 'bap'];
+
+function createButton (menu) {
+    var btn1 = document.createElement('button');
+    btn1.innerText = menu;
+    return btn1
+};
+
+fetch('https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=873c6628dea5448a91cb72febe1f297e')
+.then(res => {
+    return res.json()
+})
+.then(res => {
+        for (var i = 0; i<res.length; i++) {
+            var newButton = createButton(res[i].title)
+            recipeNames.append(newButton);
+            setOnclick(newButton, res[i].id);
+        }
+    } 
+)
+
+function setOnclick (btn1, id) {
+    var url = 'https://api.spoonacular.com/recipes/' + id + '/summary' + '?apiKey=873c6628dea5448a91cb72febe1f297e'
+    fetch(url)
+    .then(res => {
+        return res.json()
+    })
+    .then(res => {
+        // btn1.addEventListener('click', function() {
+        $(btn1).on('click', function() {
+            displayDetails(res.summary); 
+        })
+    });
+}
+
+function displayDetails(summary){
+    var detailViewer = $('#detail-viewer');
+    detailViewer.html(summary);
+}
